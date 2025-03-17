134ae10edd9f243e6f2446a3dafbffd348f020a5
Davina Leong <davina.leong.sy@gmail.com>
2025-03-17 22:00:09 +0800

    WIP setup auth layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TempView from '../views/TempView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'temp',
+      component: TempView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
   ],
 })
---
+++ src/views/TempView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/register">Register</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+}
+</style>
